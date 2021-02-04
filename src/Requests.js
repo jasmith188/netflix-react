a4c4513ca2004449b11c7c08954557d5c22e0383
James Smith <jasmith.188@gmail.com>
2021-02-04 00:17:20 -0500

    finish rows using all genres from API

--- src/Requests.js
+++ src/Requests.js
@@ -2,7 +2,7 @@ const API_KEY = 'ae9ae8865cf4dfc4eaf095a23aca6774';
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOrginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
